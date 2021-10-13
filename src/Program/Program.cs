32cad8a2bbd272e5e8052b52388fa37616113a73
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-10-13 20:17:11 -0300

    Aplicacion de Creator

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -12,18 +12,15 @@ namespace Full_GRASP_And_SOLID
 {
     public class Program
     {
-        private static List<Product> productCatalog = new List<Product>();
-
-        private static List<Equipment> equipmentCatalog = new List<Equipment>();
-
         public static void Main(string[] args)
         {
-            PopulateCatalogs();
+            Catalog catalog = new Catalog();
+            PopulateCatalogs(catalog);
 
             Recipe recipe = new Recipe();
-            recipe.FinalProduct = GetProduct("Café con leche");
-            recipe.AddStep(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120);
-            recipe.AddStep(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60);
+            recipe.FinalProduct = catalog.GetProduct("Café con leche");
+            recipe.AddStep(catalog.GetProduct("Café"), 100, catalog.GetEquipment("Cafetera"), 120);
+            recipe.AddStep(catalog.GetProduct("Leche"), 200, catalog.GetEquipment("Hervidor"), 60);
 
             IPrinter printer;
             printer = new ConsolePrinter();
@@ -32,46 +29,14 @@ namespace Full_GRASP_And_SOLID
             printer.PrintRecipe(recipe);
         }
 
-        private static void PopulateCatalogs()
+        private static void PopulateCatalogs(Catalog catalog)
         {
-            AddProductToCatalog("Café", 100);
-            AddProductToCatalog("Leche", 200);
-            AddProductToCatalog("Café con leche", 300);
+            catalog.AddProductToCatalog("Café", 100);
+            catalog.AddProductToCatalog("Leche", 200);
+            catalog.AddProductToCatalog("Café con leche", 300);
 
-            AddEquipmentToCatalog("Cafetera", 1000);
-            AddEquipmentToCatalog("Hervidor", 2000);
-        }
-
-        private static void AddProductToCatalog(string description, double unitCost)
-        {
-            productCatalog.Add(new Product(description, unitCost));
-        }
-
-        private static void AddEquipmentToCatalog(string description, double hourlyCost)
-        {
-            equipmentCatalog.Add(new Equipment(description, hourlyCost));
-        }
-
-        private static Product ProductAt(int index)
-        {
-            return productCatalog[index] as Product;
-        }
-
-        private static Equipment EquipmentAt(int index)
-        {
-            return equipmentCatalog[index] as Equipment;
-        }
-
-        private static Product GetProduct(string description)
-        {
-            var query = from Product product in productCatalog where product.Description == description select product;
-            return query.FirstOrDefault();
-        }
-
-        private static Equipment GetEquipment(string description)
-        {
-            var query = from Equipment equipment in equipmentCatalog where equipment.Description == description select equipment;
-            return query.FirstOrDefault();
+            catalog.AddEquipmentToCatalog("Cafetera", 1000);
+            catalog.AddEquipmentToCatalog("Hervidor", 2000);
         }
     }
 }
